469e3683c4c36b8147723f10a767ce87fe572cf1
huinaibing <1575044238@qq.com>
2025-04-24 15:29:09 +0800

    add some files

---
+++ python_code/deeplearning/utilclass.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class Util:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def relu(z):
+        return np.where(z > 0, z, 0)
+
+    @staticmethod
+    def sigmoid(z):
+        return 1 / (1 + np.exp(-z))
+
+    @staticmethod
+    def swish(z):
+        return z * Util.sigmoid(z)
+
+    @staticmethod
+    def one_simple_layer(x, dim_in, dim_out, activation=swish):
+        """
+
+        :param dim_out:
+        :param dim_in:
+        :param x:
+        :param activation:
+        :return:
+        """
+        return activation(np.random.randn(dim_out, dim_in) @ x + np.random.randn(dim_out, 1))
