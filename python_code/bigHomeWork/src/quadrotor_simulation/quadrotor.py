14eca6394dece8d96e4eea9dd1357699c21c37bf
huinaibing <huinaibing@gmail.com>
2025-06-25 16:40:33 +0800

    calculate traslational force

--- python_code/bigHomeWork/src/quadrotor_simulation/quadrotor.py
+++ python_code/bigHomeWork/src/quadrotor_simulation/quadrotor.py
@@ -1,11 +1,20 @@
 import simulator_config.quadrotor_cfg as quad_cfg
 import simulator_config.environment_cfg as env_cfg
 import numpy as np
-
+from xqy_utils.xqy_utils import XQYUtils as utils
 
 class Quadrotor:
     """
     专门用来处理无人机仿真的类
+    包含以下要素
+    1. 初始化各种参数（init）
+    2. 计算无人机的受力
+        - 这一部分是在参考系下做的，需要格外注意z轴取向向下
+        - 受力、速度、角速度等等通通用这个坐标系，
+        - 但是位置感觉用不了，我会区别更新，注意这里！！！
+    
+        
+    n. 和gym交互的api
     """
     def __init__(self):
         # 无人机的参数
@@ -33,6 +42,7 @@ class Quadrotor:
             ],
             dtype=np.float32
         ) # shape = (3, 1)
+        # 位置不使用那个z向下坐标系！！！！
 
         self.angles = np.matrix(
             [
@@ -50,16 +60,24 @@ class Quadrotor:
                 [env_cfg.environment_init_cfg["vzInit"]]
             ]
         ) # shape = (3, 1)
+        # 速度使用z向下坐标系！
 
         # 旋翼角速度
         # shape = (4,)
         # shape和上面不一样！！！！！！！！！！！！！！
+        # 角速度如果为正数，代表升力向上（z轴负方向！！！）
         self.omega = env_cfg.environment_init_cfg["omegaInit"]
 
+    ########################################
+    # 计算无人机的各种受力的部分
+    ########################################
+    
     def get_lift_force_in_body(self):
         """
         获取无人机的每一个旋翼的力
         在body坐标系！！！！
+        注意这里的力为正的话表示向上，但是我们坐标系的z轴是向下的
+        在get_f_lab()中会处理这个符号问题
         :return np.array: shape=(4,)
         """
         # 从角速度到升力
@@ -72,6 +90,54 @@ class Quadrotor:
                 f_body[i] *= -1
 
         return f_body
+    
+    def get_f_lab(self):
+        """
+        把升力转化到实验室坐标系
+        注意z轴是向下的！！！！！！！！！
+        注意z轴是向下的！！！！！！！！！
+        注意z轴是向下的！！！！！！！！！
+        :return np.array: shape=(3, 1) 
+        """
+        f_body = self.get_lift_force_in_body()
+        f_matrix_body = np.zeros((3, 1), dtype=np.float32)
+        # 注意下面这里取了负号
+        f_matrix_body[2, 0] = -np.sum(f_body)
+
+        # 旋转矩阵
+        # 这里要转置矩阵，看README
+        R_v_v1_T = utils.get_R_v_v1(self.angles[0, 0]).T
+        R_v1_v2_T = utils.get_R_v1_v2(self.angles[1, 0]).T
+        R_v2_b_T = utils.get_v2_b(self.angles[2, 0]).T
+
+        return R_v_v1_T * R_v1_v2_T * R_v2_b_T * f_matrix_body
+    
+    def get_gravity_lab(self):
+        """
+        算重力
+        z轴是向下的！！！！！！！！！
+        z轴是向下的！！！！！！！！
+        z轴是向下的！！！！！！！！！
+        :return np.array: shape=(3, 1)
+        """
+        gravity_lab = np.zeros((3, 1), dtype=np.float32)
+        gravity_lab[2, 0] = self.mass * self.gravity
+        return gravity_lab
+    
+    def get_drag_lab(self):
+        """
+        空气阻力
+        :return np.array: shape=(3, 1)
+        """
+        return -self.cD * self.velocity
+    
+    #
+    # end：计算无人机的各种受力的部分
+    #
+    
+    #########################################
+    # 和gym交互的部分
+    #########################################
 
     def get_state(self):
         """
@@ -99,6 +165,8 @@ class Quadrotor:
         记得先set_omega再move!!!!!!!!!!!!
         记得先set_omega再move!!!!!!!!!!!!
         记得先set_omega再move!!!!!!!!!!!!
-        更新方程见README
-        全代码最重要的一段
         """
+
+    #
+    # end：和gym交互的部分
+    #
